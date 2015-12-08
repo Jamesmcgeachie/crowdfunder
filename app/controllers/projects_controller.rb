3016b35e22641fddd94324246d0aa0581d1814a8
rex yun <chgchg123@naver.com>
2015-12-08 16:38:31 -0500

    basic structure of projects crud and view

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,28 @@
+class ProjectsController < ApplicationController
+  def index
+  	@projects = Project.all
+  end
+
+  def show
+  	@project = Project.find(params[:id])
+  end
+
+  def new
+  	@project = Project.new
+  end
+
+  def edit
+  	@project = Project.find(params[:id])
+  end
+
+  def create
+  	@project = Project.new(project_params)
+    @project.save
+    redirect_to projects_url
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:name, :description, :funding_goal)
+  end
+end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ActiveRecord::Base
   belongs_to :user
   has_many :rewards
+
+  validates :name, :description, :funding_goal, presence: true
 end
