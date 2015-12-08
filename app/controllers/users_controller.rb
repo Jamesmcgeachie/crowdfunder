a62bc2b1a448e21074ea293230de038698a934b6
James McGeachie <jamesmcgeachie10@gmail.com>
2015-12-08 18:23:26 -0500

    Added edit and update actions for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  before_action :get_user, only: [:show, :edit, :update]
+
   def index
     @users = User.all
   end
@@ -11,6 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      auto_login(@user)
       redirect_to users_path
     else
       render :new
@@ -18,15 +21,25 @@ class UsersController < ApplicationController
   end
 
   def show
+
   end
 
   def edit
   end
 
   def update
+    if @user.update_attributes(user_params)
+      redirect_to users_path
+    else
+      render :edit
+    end
   end
 
   private
+
+  def get_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
