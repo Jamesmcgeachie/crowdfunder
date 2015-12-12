113e213f755ce378562c759ed11060a3257fbf05
James McGeachie <jamesmcgeachie10@gmail.com>
2015-12-11 19:27:44 -0500

    Cleaned up model associations in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,11 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
 	has_many :projects
-	#has_many :backed_projects, through: :pledges, source: :project, class_name: "Project"
 	has_many :pledges
 	has_many :reviews
-
-
   has_many :rewards, through: :pledges
   has_many :backed_projects, through: :rewards, source: :project, class_name: "Project"
 
-	# Below is how I'm trying to access a project object for the user profile
-	# has_many :projects_pledged, through: :pledges, source:
 
 	validates :password, length: { minimum: 3 }
 	validates :password, confirmation: true
